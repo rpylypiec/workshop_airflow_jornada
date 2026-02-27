cc0046b448e60e9185947afbd98697f638ca3d71
Raphael Pylypiec <rpylypiec@outlook.com>
2026-02-27 00:50:33 -0300

    Update

---
+++ exemplo_00.py
@@ -0,0 +1,24 @@
+from time import sleep
+
+def primeira_atividade():
+    print("Minha primeira atividade!")
+    sleep(2)
+
+def segunda_atividade():
+    print("Minha segunda atividade!")
+    sleep(2)
+
+def terceira_atividade():
+    print("Minha terceira atividade!")
+    sleep(2)
+
+
+def pipeline():
+    primeira_atividade()
+    segunda_atividade()
+    terceira_atividade()
+    print("Pipeline finalizou!")
+
+if __name__ == "__main__":
+    while True:
+        pipeline()
---
+++ frontend/pipeline/pipeline.py
@@ -0,0 +1,29 @@
+from time import sleep
+
+from loguru import logger
+
+logger.add("execution_logs.log", format="{time} - {message}", level="INFO", rotation="1 Day" )
+
+def primeira_atividade():
+    logger.info("Minha primeira atividade!")
+    sleep(2)
+
+def segunda_atividade():
+    logger.info("Minha segunda atividade!")
+    sleep(2)
+
+def terceira_atividade():
+    logger.info("Minha terceira atividade!")
+    sleep(2)
+
+
+def pipeline():
+    primeira_atividade()
+    segunda_atividade()
+    terceira_atividade()
+    logger.info("Pipeline finalizou!")
+
+if __name__ == "__main__":
+    while True:
+        pipeline()
+        sleep(10)
